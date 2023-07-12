489d7097417031768c476fd280a342056f09fead
Emerole99 <ikennaemerole5@gmail.com>
2023-07-12 14:19:03 +0100

    update

---
+++ js/signup.js
@@ -0,0 +1,63 @@
+const firstNameInput = document.querySelector("#First");
+const lastNameInput = document.querySelector("#Last");
+const emailInput = document.querySelector("#gmail");
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#change");
+const form = document.querySelector(".card");
+const message = document.querySelector('.message');
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const confirmPassword = confirmPasswordInput.value;
+
+  if (!firstName || !lastName || !email || !password || !confirmPassword) {
+    message.textContent ="Please fill the form correctly.";
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    message.textContent="Your passwords don't match.";
+    return;
+  }
+
+  const userLog = {
+    first_name: firstName,
+    last_name: lastName,
+    email: email,
+    password: password,
+  };
+
+
+
+
+
+
+  
+  try {
+    const response = await fetch("http://localhost:5000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userLog),
+    });
+
+    if (!response.ok) {
+      const jsonData = await response.json();
+      throw new Error(jsonData.error);
+    }
+
+    const jsonData = await response.json();
+    alert(jsonData.message);
+  } catch (error) {
+   message.textContent = error.message
+  }
+  
+
+  form.reset()
+});
---
+++ node/app.js
@@ -0,0 +1,142 @@
+const express = require("express");
+const morgan = require("morgan");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
+const db = require("./db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors({
+    origin: ["http://127.0.0.1:5500"],
+    credentials: true
+}));
+
+app.post("/signup", async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+  console.log("Signup request received");
+
+  try {
+    if (!first_name) throw Error("Please insert your first name");
+    if (!last_name) throw Error("Please insert your last name");
+    if (!email) throw Error("Please insert your email");
+    if (!password) throw Error("Please insert your password");
+
+    // Check if the email already exists in the database
+    const existingUser = await db.execute(
+      "SELECT * FROM user WHERE email = ?",
+      [email]
+    );
+    if (existingUser[0].length > 0) {
+      throw Error("Email already exists. Please choose a different email.");
+    }
+
+    const id = uuid.v4();
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    let sql = `INSERT INTO user (id, first_name, last_name, email, password)
+                 VALUES (?, ?, ?, ?, ?)`;
+
+    await db.execute(sql, [id, first_name, last_name, email, hash]);
+    res.status(200).json({ message: "Your account has been created" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (!email) throw Error('Please insert your email');
+    if (!password) throw Error('Please insert your password');
+
+    // Check if the email and password match the user in the database
+    const user = await db.execute('SELECT * FROM user WHERE email = ?', [email]);
+
+    if (user[0].length === 0) {
+      throw Error('Invalid email or password');
+    }
+
+    const { id, first_name, last_name, password: storedPassword } = user[0][0];
+
+    const isMatch = await bcrypt.compare(password, storedPassword);
+    if (!isMatch) {
+      throw Error('Invalid email or password');
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ id, email }, JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ id, first_name, last_name, token, message: 'Login successful' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
+  // ...
+
+// ...
+
+app.patch('/changepassword', async (req, res) => {
+  const { email, newPassword } = req.body;
+
+  try {
+    if (!email) throw Error('Please insert your email');
+    if (!newPassword) throw Error('Please insert your new password');
+
+    // Check if the email exists in the database
+    const user = await db.execute('SELECT * FROM auth WHERE email = ?', [email]);
+
+    if (user[0].length === 0) {
+      throw Error('Email not found. Please enter a valid email.');
+    }
+
+    // Update the user's password in the database
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(newPassword, salt);
+
+    const sql = 'UPDATE auth SET password = ? WHERE email = ?';
+    await db.execute(sql, [hash, email]);
+
+    res.status(200).json({ message: 'Password has been changed' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// ...
+app.get('/user/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Fetch user information from the database based on the provided ID
+    const user = await db.execute('SELECT * FROM auth WHERE id = ?', [id]);
+
+    if (user[0].length === 0) {
+      throw Error('User not found.');
+    }
+
+    const { first_name, last_name, email } = user[0][0];
+    res.status(200).json({ id, first_name, last_name , email});
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+
+// ...
+
+
+  
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
